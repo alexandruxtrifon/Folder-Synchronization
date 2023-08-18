dc419db9cb0c8a1035a13c31ba76e0858efd3538
Alex <Alex@DESKTOP-FA797RR>
2023-08-18 21:41:15 +0300

    -getting hash code md5 from every file

--- Folder Synchronization/Program.cs
+++ Folder Synchronization/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
-
-//string root = @"C:\Users\Alex\Desktop\root\";
-//string destination = @"C:\Users\Alex\Desktop\destination\";
-Console.Write("Enter the source folder path: ");
-string root = Console.ReadLine();
+string root = @"C:\Users\Alex\Desktop\root\";
+string destination = @"C:\Users\Alex\Desktop\destination\";
+//Console.Write("Enter the source folder path: ");
+//string root = Console.ReadLine();
 string[] files = Directory.GetFiles(root);
-Console.Write("\nEnter the destination folder path: ");
-string destination = Console.ReadLine();
+//Console.Write("\nEnter the destination folder path: ");
+//string destination = Console.ReadLine();
+var md5 = MD5.Create();
+
 foreach (string file in files)
 {
     File.Copy(file, $"{destination}{Path.GetFileName(file)}", true);
+    var stream = File.OpenRead(file);
+    var hash = md5.ComputeHash(stream);
+    Console.WriteLine(BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant());
+}
+
+class Item
+{
+
 }
