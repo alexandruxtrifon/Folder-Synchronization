b7aae61f74c784e84fb9b7de2910f03de5f19433
Alex <Alex@DESKTOP-FA797RR>
2023-08-18 14:49:08 +0300

    folder path input from keyboard

--- Folder Synchronization/Program.cs
+++ Folder Synchronization/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System.Globalization;
 
 
-string root = @"C:\Users\Alex\Desktop\root\";
-string destination = @"C:\Users\Alex\Desktop\destination\";
+//string root = @"C:\Users\Alex\Desktop\root\";
+//string destination = @"C:\Users\Alex\Desktop\destination\";
+Console.Write("Enter the source folder path: ");
+string root = Console.ReadLine();
 string[] files = Directory.GetFiles(root);
+Console.Write("\nEnter the destination folder path: ");
+string destination = Console.ReadLine();
 foreach (string file in files)
 {
     File.Copy(file, $"{destination}{Path.GetFileName(file)}", true);
