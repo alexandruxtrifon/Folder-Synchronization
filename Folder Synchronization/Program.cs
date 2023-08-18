d5b5c8d005fce1472cd75fa6e9fb9f91378d4927
Alex <Alex@DESKTOP-FA797RR>
2023-08-18 13:16:56 +0300

    -copy to destination folder

--- Folder Synchronization/Program.cs
+++ Folder Synchronization/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+
+string root = @"C:\Users\Alex\Desktop\root\";
+string destination = @"C:\Users\Alex\Desktop\destination\";
+string[] files = Directory.GetFiles(root);
+foreach (string file in files)
+{
+    File.Copy(file, $"{destination}{Path.GetFileName(file)}", true);
+}
