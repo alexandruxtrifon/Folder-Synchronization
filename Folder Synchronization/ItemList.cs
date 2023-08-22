0e0d1039eb4fa95c2277cd138476553869718684
Alex <Alex@DESKTOP-FA797RR>
2023-08-22 15:38:52 +0300

    -

---
+++ Folder Synchronization/ItemList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folder_Synchronization
+{
+    internal class ItemList : List<Item>
+    {
+        Logger _logger;
+
+        public ItemList(Logger logger)
+        { _logger = logger; }
+
+        public ItemList(ItemList itemlist)
+        {
+            this._logger = itemlist._logger;
+            foreach (var item in itemlist)
+            {
+                this.Add(item);
+            }
+        }
+    }
+}
