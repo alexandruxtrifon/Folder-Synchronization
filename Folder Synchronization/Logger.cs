daab1beb1539f1a8300c2578e900ff2a2bb32177
Alex <Alex@DESKTOP-FA797RR>
2023-08-22 13:51:34 +0300

    created logger class

---
+++ Folder Synchronization/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folder_Synchronization
+{
+    internal class Logger
+    {
+        private string consolePath { get; set; }
+        public Logger(string consolePath)
+        {
+            this.consolePath = consolePath;
+        }
+    }
+}
