198a758ab9960a6be05071ad8221bc0de8e795f4
Alex <Alex@DESKTOP-FA797RR>
2023-08-22 13:36:09 +0300

    created class

---
+++ Folder Synchronization/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folder_Synchronization
+{
+    internal class Item
+    {
+        MD5 _md5 = MD5.Create();
+        byte[] _byteHash;
+
+
+        public string path { get; set; }
+        public string name { get; }
+        public string directory { get; }
+        public string hash { get; }
+        public bool verified { get; set; } = false;
+
+        public Item()
+        {
+
+        }
+
+    }
+}
